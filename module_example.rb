5c758b6006cf23d1ed39a955ce2b467c645530ec
Neah Nieto <nrpcnieto@gmail.com>
2019-10-30 18:41:29 -0500

    Create module_example.rb for exercise

---
+++ module_example.rb
@@ -0,0 +1,40 @@
+module Drivable
+  def accelerate
+    @speed += 10
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Drivable
+  def initialize(input_options)
+    attr_accessor :fuel, :make, :model
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+
+  def honk_horn
+    puts 'Beeeeeeep!'
+  end
+end
+
+class Bike
+  include Drivable
+  def initialize(input_options)
+    attr_accessor :type, :gears
+    @type = input_options[:type]
+    @gears = input_options[:gears]
+  end
+
+  def ring_bell
+    puts 'Ring ring!'
+  end
+end
