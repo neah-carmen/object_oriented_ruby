f80413c310226b19c6cb2146a719968ec51b2410
Neah Nieto <nrpcnieto@gmail.com>
2019-10-30 19:25:22 -0500

    Separate classes and modules and used require for exercise

---
+++ email_reportable.rb
@@ -0,0 +1,11 @@
+module EmailReportable
+  def send_report
+    puts "Sending email..."
+    # code to actually send email
+    puts "Email sent!"
+  end
+
+  def receive_report
+    puts "Do something"
+  end
+end
